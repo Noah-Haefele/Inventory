8371da60d6e1ff2554757327dabfd80f9e871a4d
Noah <noah.haefele@gmx.de>
2026-02-22 19:15:40 +0100

    fixed two mistakes

--- static/js/event_detail.js
+++ static/js/event_detail.js
@@ -19,7 +19,7 @@ class EventDatailManager {
 
     attachEventListeners() {
         if (this.openModalButton) {
-            this.openModalButton.onclick = () => EventDatailManager.toggleModal(true);
+            this.openModalButton.onclick = () => this.toggleModal(true);
         }
         if (this.confirmAddButton) {
             this.confirmAddButton.onclick = () => this.assignSelectedItems(this.eventId);
